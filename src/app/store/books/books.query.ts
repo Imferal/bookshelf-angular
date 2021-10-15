7f8457f932bd6771bdbd15eb7a2607ff41759ca7
imferal <i.m.feral@gmail.com>
2021-10-15 19:13:01 +0300

    211015_4

--- src/app/store/books/books.query.ts
+++ src/app/store/books/books.query.ts
@@ -1,22 +1,26 @@
 import {QueryEntity} from "@datorama/akita";
 import {BooksState, BooksStore} from "./books.store";
-import {combineLatest, Observable, Subscription, zip} from "rxjs";
+import {combineLatest, Observable} from "rxjs";
 import {Injectable, OnInit} from "@angular/core";
 import {Book} from "./book.model";
 import {GenresQuery} from "../genres/genres.query";
-import {Genre} from "../genres/genre.model";
 import {map} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class BooksQuery extends QueryEntity<BooksState> implements OnInit {
   books$: Observable<Book[]> = this.selectAll()
-  combinedStream: Observable<Book[]> = combineLatest(this.books$(ф), this.genresQuery.genres$)
+  combinedStream: Observable<Book[]> = combineLatest([
+    this.books$,
+    /** Жанры превращаем в объекты с ключами, взятыми из значений id */
+    this.genresQuery.selectAll({asObject: true})
+  ])
     .pipe(
       map(([books, genres]) => {
         return books.map(book => {
           return {
             ...book,
-            genres: genres.filter(genre => book.genreIds.includes(genre.id))
+            /** Возвращаем значения жанров с ключами, совпадающими с genreIds в книге */
+            genres: book.genreIds.map(genreId => genres[genreId])
           }
         })
       })
