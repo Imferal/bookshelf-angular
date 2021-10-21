fc4be7eb01bf9a3352b481f16deabf2351c51e5b
imferal <i.m.feral@gmail.com>
2021-10-21 18:18:47 +0300

    211021_2

--- src/app/modules/home/components/filter/filter.component.ts
+++ src/app/modules/home/components/filter/filter.component.ts
@@ -3,10 +3,11 @@ import {FormControl, FormGroup} from "@angular/forms";
 import {GenresQuery} from "../../../../store/genres/genres.query";
 import {NgSelectConfig} from "@ng-select/ng-select";
 import {BooksService} from "../../../../store/books/books.service";
-import {untilDestroyed} from "@ngneat/until-destroy";
+import {UntilDestroy, untilDestroyed} from "@ngneat/until-destroy";
 import {BooksFilterService} from "../../../../shared/services/books-filter.service";
 import {searchFilter} from "akita-filters-plugin";
 
+@UntilDestroy()
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
@@ -28,18 +29,6 @@ export class FilterComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.form.controls.author.valueChanges.pipe(untilDestroyed(this)).subscribe((author: string) => {
-      if (author) {
-        this.booksFilter.setFilter({
-          id: 'author',
-          value: author,
-          predicate: entity => searchFilter(author, entity)
-        })
-      } else {
-        this.booksFilter.removeFilter('author')
-      }
-    })
-
     /** Инициализация формы добавления новой книги */
     this.form = new FormGroup({
       author: new FormControl(null),
@@ -48,6 +37,72 @@ export class FilterComponent implements OnInit {
       yearTo: new FormControl(null),
       genreIds: new FormControl(null),
     })
+
+    /** Фильтрация по автору */
+    this.form.controls.author.valueChanges.pipe(untilDestroyed(this)).subscribe((author: string) => {
+      if (author) {
+        this.booksFilter.setFilter({
+          id: 'author',
+          value: author,
+          predicate: entity => entity.author.toLocaleLowerCase().includes(author.toLocaleLowerCase())
+        })
+      } else {
+        this.booksFilter.removeFilter('author')
+      }
+    })
+
+    /** Фильтрация по вхождению */
+    this.form.controls.text.valueChanges.pipe(untilDestroyed(this)).subscribe((text: string) => {
+      if (text) {
+        this.booksFilter.setFilter({
+          id: 'text',
+          value: text,
+          predicate: entity => entity.name.toLocaleLowerCase().includes(text.toLocaleLowerCase()) || (entity.description?.toLocaleLowerCase().includes(text.toLocaleLowerCase()) || false)
+        })
+      } else {
+        this.booksFilter.removeFilter('text')
+      }
+    })
+
+    /** Фильтрация по году от */
+    this.form.controls.yearFrom.valueChanges.pipe(untilDestroyed(this)).subscribe((yearFrom: string) => {
+      if (yearFrom) {
+        this.booksFilter.setFilter({
+          id: 'yearFrom',
+          value: yearFrom,
+          predicate: entity => entity.year >= +yearFrom
+        })
+      } else {
+        this.booksFilter.removeFilter('yearFrom')
+      }
+    })
+
+    /** Фильтрация по году до */
+    this.form.controls.yearTo.valueChanges.pipe(untilDestroyed(this)).subscribe((yearTo: string) => {
+      if (yearTo) {
+        this.booksFilter.setFilter({
+          id: 'yearTo',
+          value: yearTo,
+          predicate: entity => entity.year <= +yearTo
+        })
+      } else {
+        this.booksFilter.removeFilter('yearTo')
+      }
+    })
+
+    /** Фильтрация по жанру */
+    this.form.controls.genreIds.valueChanges.pipe(untilDestroyed(this)).subscribe((genreIds: string[]) => {
+      if (genreIds) {
+        this.booksFilter.setFilter({
+          id: 'genreIds',
+          value: genreIds,
+          predicate: entity => genreIds!.every(genreId => entity.genreIds.includes(genreId))
+        })
+      } else {
+        this.booksFilter.removeFilter('genreIds')
+      }
+    })
+
   }
 
   showForm() {
